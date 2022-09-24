be9ded4c3189d4409e94dbdc21bc873c0420d11f
MT-0xf <matsumoto0xf@gmail.com>
2022-09-24 22:15:35 +0900

    不要行 削除

--- frontend/pages/recipe-register/index.tsx
+++ frontend/pages/recipe-register/index.tsx
@@ -1,36 +1,8 @@
 import type { NextPage } from 'next'
 import Header from '../components/header'
-import styled from 'styled-components'
 import RecipeRegisterForm from '../components/recipeRegisterForm'
 
 const RECIPE_REGISTER: NextPage = () => {
-    const BACKGROUND = styled.div`
-        width: 100%;
-        height: 100%;
-        background-color: black;
-        display:inline-block;
-        position: relative;
-    `
-
-    const BACKGROUND_IMG = styled.img`
-        width: 100%;
-        height: 1200px;
-        display: inline-block;
-        opacity: 0.5;
-        object-fit: cover;
-    `
-
-    const MAIN_AREA = styled.div`
-        width: 800px;
-        height: 1000px;
-        padding: 145px;
-        background-color: white;
-        opacity: 0.7;
-        position: absolute;
-        top: 7%;
-        left: 18%;
-    `;
-
     return (
         <div>
             <Header />
